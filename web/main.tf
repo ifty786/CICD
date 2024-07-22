18df98012e5b640e9a4336315c6422da82a33a2b
ifty786 <sgg_317@hotmail.com>
2024-07-22 14:57:43 +0100

    Update main.tf

--- web/main.tf
+++ web/main.tf
@@ -1,6 +1,6 @@
 #ec2
 resource "aws_instance" "server" {
-  ami = "ami-0b72821e2f351e396"
+  ami = "ami-04a81a99f5ec58529"
   instance_type = "t2.micro"
   subnet_id = var.sn
   security_groups = [var.sg]
